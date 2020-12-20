db12ddd53386fe1d468a830914953bbbb2065d3e
JulioCesarAM <fhoenixeterno@gmail.com>
2020-12-20 19:54:27 +0100

    Update Die.java

--- src/main/java/juldierau/Die.java
+++ src/main/java/juldierau/Die.java
@@ -6,6 +6,7 @@ public class Die {
 
 	static public double getDoubleValue(int _numLimite) {
 		return (Math.random() * _numLimite) + 1;
+	}
 
 	public static int getDiscretValue(int start, int end) {
 		
