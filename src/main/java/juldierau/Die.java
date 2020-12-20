8003236c525ddf7442f8ece660d1c8c65b4e4059
Guiteau <diegodam999@gmail.com>
2020-12-20 18:47:03 +0000

    Update Die.java

--- src/main/java/juldierau/Die.java
+++ src/main/java/juldierau/Die.java
@@ -1,5 +1,16 @@
 package juldierau;
 
 public class Die {
+	
+	
+	public static int getDiscretValue(int start, int end) {
+		
+		int resultado = 0;
+		
+		resultado = start + (int)(Math.random() * ((end - start) + 1));		
+		
+		return resultado;
+		
+	}
 
 }
